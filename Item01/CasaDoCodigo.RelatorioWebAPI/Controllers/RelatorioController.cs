8592533d8960f0a90617f4c4a16d53e672cfa8be
marcelooliveira <mclricardo@gmail.com>
2019-06-26 15:28:49 -0300

    1307-aspnetcoreparte4identityserver-video1.4-Os Métodos HTTP.trec

--- Item01/CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
+++ Item01/CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -2,47 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CasaDoCodigo.RelatorioWebAPI.Controllers
 {
-    //OPERAÇÃO CRUD
-    //C = CREATE
-    //R = RETRIEVE = OBTER
-    //U = UPDATE = ATUALIZAR
-    //D = DELETE
-
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class RelatorioController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Relatorio
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Relatorio/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Relatorio
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Relatorio/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
